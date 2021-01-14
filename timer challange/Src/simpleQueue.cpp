393cdb20d32d60f46ee5900aaf0e1051152ce6d3
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2021-01-14 14:43:31 +0100

    test new queue system

--- timer challange/Src/simpleQueue.cpp
+++ timer challange/Src/simpleQueue.cpp
@@ -1,28 +1,34 @@
 #include "simpleQueue.h"
+// static char msgBuf[80];
 
 void addToQueue(SimpleQueue** queue, char* value){
+     SimpleQueue* newElement = (SimpleQueue *)malloc(sizeof(SimpleQueue));
+     strcpy(newElement->value, value); 
+     newElement->nextSQ = NULL;
     if(*queue == NULL){
-        SimpleQueue* newElement = (SimpleQueue *)malloc(sizeof(SimpleQueue));
-
-        newElement->value = value;
-        newElement->nextSQ = NULL;
         *queue = newElement;
-    }else{
-        SimpleQueue* newElement = *queue;
-        while(newElement->nextSQ != NULL){      
-            newElement = newElement->nextSQ;
-        }
-        newElement->nextSQ = (SimpleQueue *)malloc(sizeof(SimpleQueue));
-        newElement->nextSQ->value = value;
-        newElement->nextSQ->nextSQ = NULL;
+//  sprintf(msgBuf, "%s", "adding to empty");
     }
+    else
+    {
+//  sprintf(msgBuf, "%s", "adding to existing");
+
+        SimpleQueue* currentElement = *queue;
+        while(currentElement->nextSQ != NULL)
+        {      
+            currentElement = currentElement->nextSQ;
+        }
+        currentElement->nextSQ = newElement;
+    }
+//   HAL_UART_Transmit(&huart2, (uint8_t *)msgBuf, strlen(msgBuf), HAL_MAX_DELAY);
 }
 
 char* retrieveFromQueue(SimpleQueue** queue){
     if(*queue == NULL){
         //error:empty ingekort voor efficientie
-        return (char*)"e:e\r\n";
+        return (char*)"e:e";
     }
+   
     SimpleQueue* next_item = (*queue)->nextSQ;
     char* tmpVal = (*queue)->value;
     free(*queue);
